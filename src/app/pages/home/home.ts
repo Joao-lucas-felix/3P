7260742a06916456243da721835d69972cc6896a
João Lucas Felix de Macena <jlmacena06@gmail.com>
2026-02-13 09:57:01 -0300

    feature/ home page initial impl

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../core/components/navbar/navbar'
+@Component({
+  selector: 'app-home',
+  imports: [Navbar],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
