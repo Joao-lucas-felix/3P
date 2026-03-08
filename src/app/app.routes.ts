38ffcd678b990450c8cf934c289fd374666973bd
João Lucas Felix de Macena <jlmacena06@gmail.com>
2026-03-08 15:14:58 -0300

    feature/ adding a footer and intro text

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '', redirectTo: '/3P/#/home', pathMatch: 'full' },
     {
-        path: 'home', 
+        path: '/3P/#/home', 
         title: 'Home',
         loadComponent: () => import("./pages/home/home").then( p => p.Home ),
     }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../core/components/navbar/navbar'
+import { Footer } from '../../core/components/footer/footer';
 @Component({
   selector: 'app-home',
-  imports: [Navbar],
+  imports: [Navbar, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
